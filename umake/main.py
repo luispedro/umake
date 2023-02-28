f677498c97b04e3c50f8d84bde971c0d4897ec16
Luis Pedro Coelho <luis@luispedro.org>
2023-02-28 11:50:23 +0800

    ENH First step
    Makes a PNG from an SVG file

---
+++ umake/main.py
@@ -0,0 +1,48 @@
+import subprocess
+import sys
+import pathlib
+
+
+def convert_with_inkscape(target, src):
+    subprocess.check_call(
+            ['inkscape'
+            , '--export-filename', target
+            ,'--export-dpi=300'
+            ,src])
+
+converters = {
+    ('.png', '.svg') : convert_with_inkscape,
+}
+
+def main(args=None):
+    if args is None:
+        args = sys.argv
+    target = pathlib.Path(args[1])
+    if len(target.suffixes) != 1:
+        raise IOError(f'Cannot parse {target}')
+    ext1 = target.suffix
+    candidates = []
+    for c in target.absolute().parent.glob(f'{target.stem}.*'):
+        if c.suffix != target.suffix:
+            if (target.suffix, c.suffix) in converters:
+                candidates.append(c)
+    if len(candidates) == 0:
+        sys.stderr.write(f'No candidates found for {target}\n')
+        return 1
+    elif len(candidates) > 1:
+        print(f'Multiple candidates found for {target}:')
+        for i,c in enumerate(candidates):
+            print(f'[{i}] {c}')
+        print('Which one do you want to convert?')
+        i = int(input())
+        c = candidates[i]
+    else:
+        [c] = candidates
+    if target.exists():
+        print(f'File {target} already exists. Overwrite?')
+        if input() not in ['y', 'Y']:
+            return 0
+    converters[(target.suffix, c.suffix)](target, c)
+
+if __name__ == '__main__':
+    sys.exit(main())
