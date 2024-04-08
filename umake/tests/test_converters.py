2f1f5c84eaedd3e70f498bb183ac46c280451585
Luis Pedro Coelho <luis@luispedro.org>
2024-04-08 16:28:03 +1000

    ENH Add basic smoke tests

---
+++ umake/tests/test_converters.py
@@ -0,0 +1,8 @@
+from umake import converters as cs
+
+def test_has_conversions():
+    assert ('.pdf', '.doc') in cs.registry
+    assert ('.pdf', '.docx') in cs.registry
+    assert ('.pdf', '.svg') in cs.registry
+
+    assert ('.docx', '.pdf') not in cs.registry
